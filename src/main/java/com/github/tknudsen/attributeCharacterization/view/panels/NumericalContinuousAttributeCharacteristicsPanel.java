2b71c7fb5286615a270ee6e8ca0fe377b2d14457
TKnudsen <mail@juergen-bernard.de>
2026-08-14 15:38:41 +0200

    minor refinement

--- src/main/java/com/github/tknudsen/attributeCharacterization/view/panels/NumericalContinuousAttributeCharacteristicsPanel.java
+++ src/main/java/com/github/tknudsen/attributeCharacterization/view/panels/NumericalContinuousAttributeCharacteristicsPanel.java
@@ -2,8 +2,6 @@ package com.github.tknudsen.attributeCharacterization.view.panels;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Collection;
 
 import javax.swing.JCheckBox;
